5131da830566a2b4811771408544e08718076e42
mikeJT <jeerasaktarat@gmail.com>
2023-01-10 01:05:04 +0700

    AnimeCard was added

---
+++ src/components/AnimeCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <router-link 
+        class="anime-link"
+        :to="{ name: 'AnimeLayout', params: { id: anime.mal_id } }"
+    >
+        <div class="anime-card">
+            <img src= "{{ anime.images }}" alt="anime_image">
+            <h4> {{ anime.title }} </h4>
+        </div>
+    </router-link>
+</template>
+<script>
+export default {
+    name: 'AnimeCard',
+    props: {
+        anime: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<style scoped>
+.anime-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #5c3956;
+    margin-bottom: 18px;
+}
+.anime-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(255, 0, 149, 0.459);
+}
+.anime-link {
+    color: #59335e;
+    text-decoration: none;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,5 @@
-import { createStore } from 'vuex';
-
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+import { reactive } from 'vue'
+export default reactive({
+  flashMessage: '',
+  currentUser: JSON.parse(localStorage.getItem('user'))
+})
